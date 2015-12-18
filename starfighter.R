0e5d4c0c12c3667ae05f6dc6a0011cd3661c9871
richie <richie.morrisroe@gmail.com>
2015-12-18 19:13:44 +0000

    added start of parse order function

--- starfighter.R
+++ starfighter.R
@@ -1,4 +1,5 @@
 require(httr)
+require(dplyr)
 apikey <- scan("apikey.txt", what="char")
 base_url <- "https://api.stockfighter.io/ob/api"
 get_quote <- function(venue, stock) {
@@ -14,3 +15,23 @@ place_order <- function(venue, stock, body, apikey) {
     res <- httr::POST(url=url, body=body, encode="json", add_headers("X-Starfighter-Authorization"=apikey), verbose())
     
 }
+as.fnumeric <- function(x) {
+    as.numeric(as.character(x))
+}
+
+parse_quote <- function(quote) {
+    quotecontents <-
+        sapply(quote, content) %>%
+        sapply(., unlist)  %>%
+        do.call("rbind", .)
+}
+    
+test.df2  <- test.df %>% mutate(bid=as.fnumeric(bid),
+                                ask=as.fnumeric(ask),
+                                bidsize=as.fnumeric(bidSize),
+                                asksize=as.fnumeric(askSize),
+                                biddepth=as.fnumeric(bidDepth),
+                                askdepth=as.fnumeric(askDepth),
+                                last=as.fnumeric(ask),
+                                lasttrade=ymd_hms(lastTrade),
+                                quotetime=ymd_hms(quoteTime))
