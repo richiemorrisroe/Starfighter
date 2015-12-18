8071e8c70b0c8fd0f3b91049dd0e46f071a1d2de
richie <richie.morrisroe@gmail.com>
2015-12-18 17:33:14 +0000

    made create and place order funcs
    The separation is due to the fact that the order itself is POSTED to the
    server. I didn't really understand that till i wondered why the URL
    didn't seem to have everything

--- starfighter.R
+++ starfighter.R
@@ -1,10 +1,16 @@
 require(httr)
+apikey <- scan("apikey.txt", what="char")
 base_url <- "https://api.stockfighter.io/ob/api"
 get_quote <- function(venue, stock) {
     url <- paste(base_url,  "/venues/", venue, "/stocks/", stock, "/quote", sep="")
     httr::GET(url=url)
 }
-
-place_order <- function(venue, stock) {
+create_order <- function(account, venue, stock, price, qty, direction, orderType="limit"){
+    res <- list(account=account, venue=venue, stock=stock,
+                price=price, qty=qty, direction=direction, orderType=orderType)
+}
+place_order <- function(venue, stock, body, apikey) {
+    url <- paste(base_url, "/venues/", venue, "/stocks/", stock, "/orders", sep="")
+    res <- httr::POST(url=url, body=body, encode="json", add_headers("X-Starfighter-Authorization"=apikey), verbose())
     
 }
