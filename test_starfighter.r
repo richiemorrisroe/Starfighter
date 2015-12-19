c4b60c3b58a45d6edf457d575cf7b936e1b2b9e7
richie <richie.morrisroe@gmail.com>
2015-12-19 21:13:04 +0000

    some small changes

--- test_starfighter.r
+++ test_starfighter.r
@@ -4,14 +4,17 @@ source("starfighter.R")
 venue <- "TESTEX"
 stock <- "FOOBAR"
 apikey <- scan("apikey.txt", what="char")
-qty <- 100
-price <- 100
-direction <- "sell"
+
+
+direction <- "buy"
 quote <- get_quote("TESTEX", "FOOBAR")
+orderinfo <- get_bid(content(quote))
+qty <- orderinfo[2]
+price <- orderinfo[1]
 ord <- create_order(account="EXB123456", venue=venue, stock=stock, qty=qty, price=price, direction=direction, orderType="limit")
 res <- place_order(venue, stock, body=ord, apikey=apikey)
-quotes <- repeat_call(10000000, get_quote(venue, stock))
-quotes.df <- parse_quote(quotes)
+## quotes <- repeat_call(10000000, get_quote(venue, stock))
+## quotes.df <- parse_quote(quotes)
 ##fuck tests for now
 ## test_that("we can order a test stock", code={
 ##     expect_TRUE(
