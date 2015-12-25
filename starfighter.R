3a9011c5d8476aea5cf1180140ae82acd5203436
richie <richie.morrisroe@gmail.com>
2015-12-25 15:04:09 +0000

    created orderbook class
    Using S4, which should make it easier to write clearer code
    It also forces me to handle errors immediately, which makes for
    clearer error messages

--- starfighter.R
+++ starfighter.R
@@ -137,3 +137,44 @@ get_tickertape <- function(account, venue) {
     url <- paste(base_url_wss, account, "/venues/", venue, "/tickertape", sep="")
     res <- httr::GET(url, add_headers("X-Starfighter-Authorization"=apikey))
 }
+setClass("orderbook",
+         slots = list(ok="logical", venue="character",
+                      symbol="character", ts="character",
+                      bids="data.frame", asks="data.frame"))
+
+spreads.orderbook <- function(orderbook) {
+    bids <- orderbook@bids
+    asks <- orderbook@asks
+    spread <- data.frame(bid_price=bids$price,
+                         bid_qty=bids$qty,
+                         ask_price=asks$price,
+                         ask_qty=asks$qty) %>%
+        mutate(diff_price=ask_price-bid_price)
+    spread
+}
+df_or_null <- function(order, component) {
+    if(is.null(order[[component]])) {
+        order[[component]] <- data.frame(price=NULL, qty=NULL, isBuy=NULL)
+    }
+    order
+}
+        
+orderbook <- function(order) {
+    order <- df_or_null(order, "bids")
+    order <- df_or_null(order, "asks")
+    order <- order[order$ok==TRUE]
+    orderbook <- with(order,
+         new("orderbook",
+             ok=ok,
+             venue=venue,
+             symbol=symbol,
+             ts=ts,
+             bids=bids,
+             asks=asks))
+    orderbook
+}
+orderbook.loop <- list()
+for(i in 1:length(ss.parsed)) {
+    print(i)
+    orderbook.loop[[i]] <- orderbook(ss.parsed[[i]])
+}
