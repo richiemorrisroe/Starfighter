6af46f0cc14285a0823a4b7b16d0c82df6d27449
richie <richie.morrisroe@gmail.com>
2015-12-30 12:17:08 +0000

    added a test sell order

--- test_starfighter.r
+++ test_starfighter.r
@@ -12,9 +12,6 @@ orderinfo <- get_bid(content(quote))
 qty <- orderinfo[2]
 price <- orderinfo[1]
 ord <- create_order(account="EXB123456", venue=venue, stock=stock, qty=qty, price=price, direction=direction, orderType="limit")
+sell <- create_order(account="EXB123456", venue=venue, stock=stock, qty=qty, price=price+1, direction="sell", orderType="limit")
 res <- place_order(venue, stock, body=ord, apikey=apikey)
-## quotes <- repeat_call(10000000, get_quote(venue, stock))
-## quotes.df <- parse_quote(quotes)
-##fuck tests for now
-## test_that("we can order a test stock", code={
-##     expect_TRUE(
+sellres <- place_order(venue, stock, body=sell, apikey)
