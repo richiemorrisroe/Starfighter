d78bf1d45c6d3f2d2977bcf53aae889b48aee2ee
richie <richie.morrisroe@gmail.com>
2015-12-30 12:15:47 +0000

    fixed indentation

--- sell_side.R
+++ sell_side.R
@@ -1,8 +1,8 @@
 source("starfighter.R")
 source("starfighter_gm.R")
-
-level <- start_level("sell_side")
 tryCatch({
+    level <- start_level("sell_side")
+    if(content(level)$ok==TRUE) {
     account <- get_component(level, "account")
     venue <- get_component(level, "venues")
     ticker <- get_component(level, "tickers")
@@ -11,33 +11,36 @@ tryCatch({
     sell_price <- NA
     buy_price <- NA
     max_stock <- 10000*100
-    while(NAV>10000) {
+    NAV <- position$NAV
+    stocks <- get_tickers(venue=venue)
+    print(parse_response(stocks))
+    }
+    else {
+        Sys.sleep(60)
+        level <- start_level("sell_side")
+    }
+    while(NAV<10000) {
         NAV <- position$NAV
-        print(NAV)
         current_book <- tryCatch({get_orderbook(venue=venue, stock=ticker)},
                                            error=function(e) return(NA))
-        ordbook <- orderbook(parse_response(current_book))
-        if(!is.na(ordbook)) {
-        if(is.na(ordbook@bids) | is.na(ordbook@asks)) {
-            current_book <- 
-                    tryCatch(
-                    {orderbook(
-                         parse_response(get_orderbook(venue=venue, stock=ticker)))},
-                    error=function(e) return(NA))
+        ordbook <- tryCatch({orderbook(parse_response(current_book))}, error=function(e) return(NA))
+        if(is.na(ordbook)) {
+            current_book <- get_orderbook(venue=venue, stock=ticker)
+            ordbook <- orderbook(parse_response(current_book))
+    
         }
         else {
             sell_price <- min(ordbook@asks$price)-1
             buy_price <- min(ordbook@bids$price)+1
-            sell_qty <- max(ordbook@asks$qty)-1
-            buy_qty <- max(ordbook@bids$qty)-1
-        }
+            sell_qty <- min(ordbook@asks$qty)
+            buy_qty <- min(ordbook@bids$qty)
         }
         if(!is.na(sell_price)) {
             ord.buy <- create_order(account=account,
                                 venue=venue,
                                 stock=ticker,
                                 price=buy_price,
-                                qty=sell_qty,
+                                qty=buy_qty,
                                 direction="buy",
                                 orderType="limit")
 
@@ -45,8 +48,10 @@ tryCatch({
                                        body=ord.buy,
                                       apikey=apikey)
             conbuy <- parse_response(placed.buy)
-            position$bought <- position$bought+totalFilled
-            if(conbuy$totalFilled>0) {
+            
+            if(length(conbuy$fills)>0) {
+                cat("bought ", conbuy$fills$qty, " shares", "\n")
+                position$bought <- position$bought+conbuy$totalFilled
                 sell_qty <- conbuy$totalFilled
                 ord.sell <- create_order(
                     account=account,
@@ -62,15 +67,20 @@ tryCatch({
                                            apikey=apikey)
             
                 consell <- parse_response(placed.sell)
+                print(consell)
                 position$sold <- position$sold+consell$totalFilled
-                position$NAV <- NAV+(consell$price*consell$totalFilled)-(conbuy$price*conbuy$totalFilled)
-                
+                position$NAV <- position$NAV+
+                    (consell$fills$price*consell$totalFilled)-
+                    (conbuy$price*conbuy$totalFilled)
+
             }
             else {
-                Sys.sleep(20)
+                Sys.sleep(10)
             }
         }
-    }
+ }       
 }, finally=change_instance(level, "stop"))
+
+
 
         
