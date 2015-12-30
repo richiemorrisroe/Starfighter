00b9260fbef6971f12d9750eb2ab90a42dc4fa5d
richie <richie.morrisroe@gmail.com>
2015-12-30 12:15:21 +0000

    fixed indentation

--- chock_a_block.R
+++ chock_a_block.R
@@ -4,54 +4,54 @@ library(lubridate)
 source("starfighter_gm.R")
 source("starfighter.R")
 tryCatch( {
-first <- start_level("chock_a_block")
-contents_level <- content(first)
-venue <- unlist(contents_level[["venues"]])
-ticker <- unlist(contents_level[["tickers"]])
-instance <- unlist(contents_level[["instanceId"]])
-account <- unlist(contents_level[["account"]])
-daylength <- unlist(contents_level[["secondsPerTradingDay"]])
+    first <- start_level("chock_a_block")
+    contents_level <- content(first)
+    venue <- unlist(contents_level[["venues"]])
+    ticker <- unlist(contents_level[["tickers"]])
+    instance <- unlist(contents_level[["instanceId"]])
+    account <- unlist(contents_level[["account"]])
+    daylength <- unlist(contents_level[["secondsPerTradingDay"]])
 
-key <- scan("apikey.txt", what="char")
-fudge <- 0.00
-last_price <- 0
-total_filled <- 0
+    key <- scan("apikey.txt", what="char")
+    fudge <- 0.00
+    last_price <- 0
+    total_filled <- 0
 
-target_price <- NULL
-bid <- NULL
-## qty <- orderinfo[2]
-orderbook <- NULL
-test_qty <- c(10, 100, 1000)
-while(is.null(bid)) {
-    orderbook <- get_orderbook(venue=venue, stock=ticker) %>% parse_response()
-    print(orderbook)
-    if(is.null(orderbook$asks)) {
-        next
-    }
-    else {
-        bid <- min(orderbook[["asks"]]$price)+10
-    }
+    target_price <- NULL
+    bid <- NULL
+    ## qty <- orderinfo[2]
+    orderbook <- NULL
+    test_qty <- c(10, 100, 1000)
+    while(is.null(bid)) {
+        orderbook <- get_orderbook(venue=venue, stock=ticker) %>% parse_response()
+        print(orderbook)
+        if(is.null(orderbook$asks)) {
+            next
+        }
+        else {
+            bid <- min(orderbook[["asks"]]$price)+10
+        }
 }
-while(is.null(target_price)) {
-for(i in 1:length(test_qty)) {
-    ord <- create_order(account=account, venue=venue, stock=ticker, price=bid, qty=test_qty[i], direction="buy", orderType="fill-or-kill")
-    placed <- place_order(venue=venue, stock=ticker, body=ord, apikey=apikey)
-    placed_response <- parse_response(placed)
-    total_filled <- total_filled+placed_response$totalFilled
-    cat("just filled ", total_filled, "\n")
-    Sys.sleep(40)
-    status <- level_status(level=first)
-    stat_parsed <- parse_response(status)
-    if(!is.null(stat_parsed$flash)) {
-        risk <- stat_parsed$flash$info
-        risksplit <- unlist(strsplit(risk, "\\s"))
-        price <- risksplit[length(risksplit)]
-        target_price <- as.numeric(gsub("[^0-9]+", "", x=price))
-        cat("target price is ", target_price, "\n")
-        break
+    while(is.null(target_price)) {
+        for(i in 1:length(test_qty)) {
+            ord <- create_order(account=account, venue=venue, stock=ticker, price=bid, qty=test_qty[i], direction="buy", orderType="fill-or-kill")
+            placed <- place_order(venue=venue, stock=ticker, body=ord, apikey=apikey)
+            placed_response <- parse_response(placed)
+            total_filled <- total_filled+placed_response$totalFilled
+            cat("just filled ", total_filled, "\n")
+            Sys.sleep(40)
+            status <- level_status(level=first)
+            stat_parsed <- parse_response(status)
+            if(!is.null(stat_parsed$flash)) {
+                risk <- stat_parsed$flash$info
+                risksplit <- unlist(strsplit(risk, "\\s"))
+                price <- risksplit[length(risksplit)]
+                target_price <- as.numeric(gsub("[^0-9]+", "", x=price))
+                cat("target price is ", target_price, "\n")
+                break
+            }
+        }
     }
-}
-}
     if(!is.null(target_price)) {
         while(total_filled<100000) {
             orderbook <- get_orderbook(venue=venue, stock=ticker) %>% parse_response()
