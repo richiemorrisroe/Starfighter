9c3e7a06dad872cd0eb7e889fe3e0d2a548d9ee2
richie <richie.morrisroe@gmail.com>
2016-01-02 19:18:48 +0000

    added roxy-docs for many functions

--- starfighter.R
+++ starfighter.R
@@ -3,28 +3,67 @@ require(dplyr)
 apikey <- scan("apikey.txt", what="char")
 base_url <- "https://api.stockfighter.io/ob/api/"
 options(fractional.seconds=7)
+##' Return a quote for a given stock from a given venue
+##' Note that the quote is valid for a time in the past, and may not reflect current state of the market. Makes a HTTP request to the relevant endpoint. 
+##' @title get_quote
+##' @param venue where the stock is being traded
+##' @param stock which stoc to get quote for
+##' @param ... further arguments passed to httr::GET
+##' @return a named list with components
+##' @author richie
 get_quote <- function(venue, stock, ...) {
     url <- paste(base_url,  "venues/", venue, "/stocks/", stock, "/quote", sep="")
     res <- httr::GET(url=url, ...)
     return(res)
 }
+##' Get the state of the orderbook at a particular venue and for a particular stock
+##' This function returns a more granular picture than get_quote, with two dataframes containing price and qty available for both bid (buy) and ask (sell)
+##' @title get_orderbook
+##' @param venue a particular venue
+##' @param stock a particular stock
+##' @return a named list with components documented in orderbook-class
+##' @author richie
 get_orderbook <- function(venue, stock) {
     url <- paste(base_url, "venues/", venue, "/stocks/", stock, sep="")
     res <- httr::GET(url=url)
 }
-create_order <- function(account, venue, stock, price, qty, direction, orderType="limit"){
+##' Create a named list to be used in a POST request for trading a particular stock
+##' Really just provides a shim around creating a list for conversion to JSON
+##' @title create_order
+##' @param account trading account for this level
+##' @param venue venue for the level
+##' @param stock the stock to trade
+##' @param price price in US cents, must be integer
+##' @param qty number of shares to trade
+##' @param direction buy or sell
+##' @param ordertype one of market, limit, fill-or-kill or immediate-or-cancel
+##' @return a named list with components as per parameters to function
+##' @author richie
+create_order <- function(account, venue, stock, price, qty, direction, ordertype="limit"){
     res <- list(account=account, venue=venue, stock=stock,
-                price=price, qty=qty, direction=direction, orderType=orderType)
+                price=price, qty=qty, direction=direction, orderType=ordertype)
 }
 place_order <- function(venue, stock, body, apikey) {
     url <- paste(base_url, "/venues/", venue, "/stocks/", stock, "/orders", sep="")
     res <- httr::POST(url=url, body=body, encode="json", add_headers("X-Starfighter-Authorization"=apikey), verbose())
     
 }
+##' Convert factor variable to numeric
+##'
+##' Mostly because I dislike StringsAsFactors=FALSE
+##' @title as.fnumeric
+##' @param x a factor variable
+##' @return a numeric variable
+##' @author richie
 as.fnumeric <- function(x) {
     as.numeric(as.character(x))
 }
-
+##' Misguided parsing function
+##' See above
+##' @title 
+##' @param quote a quote object returned from the Stockfighter API
+##' @return 
+##' @author richie
 parse_quote <- function(quote) {
     quotecontents <- sapply(quote, function(x) content(x))
     numrows <- length(quotecontents)
@@ -33,13 +72,13 @@ parse_quote <- function(quote) {
     nameextractors <- tolower(names(quotecontents[[1]]))
     
     for(i in 1:length(nameextractors)) {
-        fun <- get_component(component=nameextractors[1])
+        fun <- get_component(component=nameextractors[i])
         browser()
         part <- fun(x=quotecontents)
         resmat[1:length(part),i] <- part
     }
     resmat
-}    
+}  
 
 get_component <- function(x, component) {
     res <- function (x) x[[component]]
@@ -61,8 +100,17 @@ get_component <- function(x, component) {
 
 ##man i really want this to work
 ##it doesn't though, suggesting I'm missing something subtle
+##' repeat a function n times, storing the results in a list
+##'
+##' Should really generalise this to also allow for while loops, as it would simplify the monitor function greatly
+##' @title repeat_call
+##' @param times A positive integer equal to or greater than 1. The number of times the function is called.
+##' @param call a function. Use of function(x) notation will be required for most functions
+##' @param sleep number of seconds to sleep between iterations
+##' @return a list containing the results of repeatedly evaluating call. 
+##' @author richie
 repeat_call <- function(times, call, sleep=10) {
-    reslist <- list()
+    reslist <- vector(mode="list", length=times)
     fun <- match.fun(call)
     for(i in 1:times) {
         reslist[[i]] <- fun()
@@ -72,10 +120,17 @@ repeat_call <- function(times, call, sleep=10) {
     }
     reslist
 }
-as.fnumeric <- function(x) {
-    as.numeric(as.character(x))
-}
-
+## as.fnumeric <- function(x) {
+##     as.numeric(as.character(x))
+## }
+##' Get the "current" asking price for a stock
+##'
+##' This is overcomplicated and conflates the price to pay with 
+##' @title 
+##' @param quote 
+##' @param fudge 
+##' @return 
+##' @author richie
 get_bid <- function(quote, fudge=0) {
     ask.cur <- quote$ask
     if(is.null(ask.cur)) {
@@ -94,7 +149,14 @@ get_bid <- function(quote, fudge=0) {
     state <- c(fudged_bid, qty)
     
 }
-
+##' More level specific functions!
+##' See above
+##' @title 
+##' @param venue venue
+##' @param stock ticker
+##' @param fudge how much to increase the price by
+##' @return a suggested bid and qty. 
+##' @author richie
 get_first_real_price <- function (venue, stock, fudge=0) {
     fudged_bid <- NA
     qty <- NA
@@ -106,20 +168,24 @@ get_first_real_price <- function (venue, stock, fudge=0) {
     res <- c(fudged_bid, qty)
 }
         
-## test.df2  <- test.df %>% mutate(bid=as.fnumeric(bid),
-##                                 ask=as.fnumeric(ask),
-##                                 bidsize=as.fnumeric(bidSize),
-##                                 asksize=as.fnumeric(askSize),
-##                                 biddepth=as.fnumeric(bidDepth),
-##                                 askdepth=as.fnumeric(askDepth),
-##                                 last=as.fnumeric(ask),
-##                                 lasttrade=ymd_hms(lastTrade),
-##                                 quotetime=ymd_hms(quoteTime))
+##' Parse JSON formatted text responses from the API
+##'
+##' essentially just calls content() and then jsonlite::fromJSON
+##' @title parse_response
+##' @param response the results of an API call
+##' @return a named list with the components of the response
+##' @author richie
 parse_response <- function (response) {
     content <- content(response, as="text")
     parsed <- jsonlite::fromJSON(content)
 
 }
+##' Helper function for the orderbook objects and functions
+##' 
+##' @title get_bids
+##' @param bid the bid component of an orderbook object
+##' @return either NA or a bid object
+##' @author richie
 get_bids <- function(bid) {
     if(is.null(bid)) {
         return(NA)
@@ -170,6 +236,13 @@ setClass("quote",
                     lastTrade=NA_character_,
                     quoteTime=NA_character_),
          contains="vector")
+##' Calculate the spreads between bid and ask from an orderbook object
+##'
+##' Doesn't really work right now
+##' @title spreads.orderbook
+##' @param orderbook an orderbook object
+##' @return a data.frame containing the prices and differences between them
+##' @author richie
 spreads.orderbook <- function(orderbook) {
     bids <- orderbook@bids
     asks <- orderbook@asks
@@ -192,6 +265,13 @@ spreads.orderbook <- function(orderbook) {
     }
     spread
 }
+##' utility function for ensuring that bid/ask dfs do not return null
+##' See description
+##' @title df_or_null
+##' @param order a list based including either a df of bids or asks
+##' @param component either bid or ask
+##' @return a new dataframe containing NA if component is null, else component
+##' @author richie
 df_or_null <- function(order, component) {
     if(is.null(order[[component]])) {
         order[[component]] <- data.frame(price=NA, qty=NA, isBuy=NA)
@@ -199,11 +279,17 @@ df_or_null <- function(order, component) {
     order
 }
         
+##' Function that creates a new orderbook object
+##' wrapper around a call to "new" that performs null removals and other checks
+##' @title orderbook
+##' @param order a response from the API as a named list containing all fields or NULL
+##' @return an orderbook object
+##' @author richie
 orderbook <- function(order) {
     order <- df_or_null(order, "bids")
     order <- df_or_null(order, "asks")
     if(is.null(order$error)) {
-        tsparsed <- parse_ts(order)
+        tsparsed <- lubridate::ymd_hms(order$ts)
     }
     if(is.null(order$error)) {
     orderbook <- with(order,
@@ -211,16 +297,20 @@ orderbook <- function(order) {
              ok=ok,
              venue=venue,
              symbol=symbol,
-             ymdhms=tsparsed[,1],
-             milliseconds=tsparsed[,2],
+             ymdhms=tsparsed,
+             milliseconds=NA_integer_,
              bids=bids,
              asks=asks))
     orderbook
     }
 }
-## obj_or_null <- function (object, data) {
-##     ifelse(
-quote_fields <- function(quote) {}
+##' Creates an object of type quote from a named list returned by parse_response
+##'
+##' See above
+##' @title new_quote
+##' @param quote 
+##' @return an S4 object of type quote
+##' @author richie
 new_quote <- function(quote) {
     q <- new("quote")
     quote2 <- lapply(quote, function(x) ifelse(is.null(x), NA, x))
@@ -238,16 +328,18 @@ new_quote <- function(quote) {
     q@quoteTime <- ifelse(!is.null(quote2$quoteTime), quote2$quoteTime, q@quoteTime)
     q
 }
-## orderbook.t <- list()
-## for(i in 1:length(quotes.parsed2)) {
-##     print(i)
-##     orderbook.t[[i]] <- new_quote(quotes.parsed[[i]])
-## }
+##' An almost entirely unnecessary parsing function
+##'
+##' See above
+##' @title 
+##' @param order an object that has a ts type column
+##' @param timestamp the column to convert (needs options(fractional.seconds=7+) to be useful
+##' @return a dataframe containing the ymdhms and fractional second components
+##' @author richie
 parse_ts <- function(order, timestamp) {
     myts <- lubridate::ymd_hms(as.character(order[[timestamp]]))
     split <- unlist(strsplit(as.character(order[[timestamp]]), ".", fixed=TRUE))
     millis <- stringr::str_extract(split[2], "[0-9]+")
-    browser()
     df <- return(data.frame(ymdhms=myts, milli=as.numeric(millis)))
 
 }
@@ -256,26 +348,65 @@ get_component <- function(level, component) {
     component <- unlist(levelcon[[component]])
     component
 }
+##' Return number of stocks traded at a venue
+##'
+##' See above
+##' @title get_tickers
+##' @param venue the venue to request stocks for
+##' @return a HTTP response containing the requested tickers
+##' @author richie
 get_tickers <- function(venue) {
     url <- paste(base_url, "venues/", venue, "/stocks/", sep="")
     res <- httr::GET(url, add_headers("X-Starfighter-Authorization"=apikey))
     res
 }
+##' get the status of an order
+##'
+##' Only useful for limit/market as ioc or fok return immediately
+##' @title get_order_status
+##' @param id the id returned from a previous order
+##' @param venue the venue
+##' @param stock the stock which was in the previous order
+##' @return a HTTP response indicating the status of this order
+##' @author richie
 get_order_status <- function(id, venue, stock) {
     url <- paste(base_url, "venues/", venue, "/stocks/", stock, "/orders/", id, sep="")
     res <- httr::GET(url, add_headers("X-Starfighter-Authorization"=apikey))
     res
 }
+##' Cancel an outstanding order
+##'
+##' See above
+##' @title cancel_order
+##' @param id the order id
+##' @param venue the venue
+##' @param stock the stock
+##' @return a HTTP response indicating the results of the call
+##' @author richie
 cancel_order <- function(id, venue, stock) {
     url <- paste(base_url, "venues/", venue, "/stocks/", stock, "/orders/", id, sep="")
     res <- httr::DELETE(url, add_headers("X-Starfighter-Authorization"=apikey))
     res
 }
+##' Get the status of all orders related to a trading account
+##' 
+##' @title get_all_orders
+##' @param venue trading venue
+##' @param account current account
+##' @return a HTTP response with data (formatted as JSON)
+##' @author richie
 get_all_orders <- function(venue, account) {
     url <- paste(base_url, "venues/", venue, "/accounts/", account, "/orders/", sep="")
     res <- httr::GET(url, add_headers("X-Starfighter-Authorization"=apikey))
     res
 }
+##' Convert a list of quote objects to a dataframe
+##'
+##' Lists of quote objects are returned by the monitor function. 
+##' @title qlist_to_df
+##' @param quotelist a list of quote objects
+##' @return a dataframe containing the quote data, with NA for any value that was NULL in the response
+##' @author richie
 qlist_to_df <- function(quotelist) {
     stopifnot(class(quotelist[[1]])=="quote")
     matrows <- length(quotelist)
@@ -300,6 +431,13 @@ qlist_to_df <- function(quotelist) {
                     
                                  
 }
+##' Convert a quote object to a vector
+##'
+##' See above
+##' @title as.vector.quote
+##' @param quote an object of class quote
+##' @return a vector containing the slots of the quote object
+##' @author richie
 as.vector.quote <- function(quote) {
     res <- c(quote@ok,
     quote@venue ,
@@ -314,6 +452,15 @@ as.vector.quote <- function(quote) {
     quote@lastTrade ,
     quote@quoteTime )
 }
+##' An attempt at using the websocket endpoints.
+##'
+##' it appears that httr and curl don't support wss endpoints. I suspect that with some care, an upgrade request can be sent as per the spec (which allows for a HTTP response)
+##' @title 
+##' @param account 
+##' @param venue 
+##' @param ... 
+##' @return 
+##' @author richie
 get_tickertape <- function(account, venue, ...) {
     base_ws<- "wss://api.stockfighter.io/ob/"
     url <- paste(base_ws, account, "/venues/", venue, "/tickertape", sep="")
@@ -322,4 +469,97 @@ get_tickertape <- function(account, venue, ...) {
                                       "Connection"="Upgrade",
                                       "Sec-Websocket-Version"="13"), ...)
     return(res)
+}
+monitor <- function(venue, stock, level=level) {
+    cat("Stock is ", ticker, " venue is ", venue, "\n")
+    start_time <- Sys.time()
+    ok <- TRUE
+    ##this is total overkill, but hey
+    ordlist <- vector(mode="list", length=2e5)
+    quotelist <- vector(mode="list", length=2e5)
+    i <- 1
+    tryCatch({
+        while(ok) {
+            new_time <- Sys.time()
+            orders <- try(get_orderbook(venue=venue, stock=stock))
+            quote <- get_quote(venue=venue, stock=stock)
+            ordlist[[i]] <- orders
+            quotelist[[i]] <- quote
+            cat("iteration at ", i, "\n")
+            i <- i + 1
+            ok <- content(orders)$ok
+            end_time <- Sys.time()
+            cat("time taken ", end_time-new_time, "\n")
+            ordlist
+            status <- change_instance(level, "") 
+            parsed_status <- parse_response(status)
+            if(status$state=="lost") {
+                break
+            }
+        }
+    }, finally = {
+        cat("reached finally", "\n")
+        myorders <- get_all_orders(
+        file.ord <- paste("orderlist_", args[1], ".rda", sep="")
+        file.quote <- paste("quotelist_", args[1], ".rda", sep="")
+        save(ordlist, file=file.ord)
+        save(quotelist, file=file.quote)
+        change_instance(level=level, "stop")
+        end_time <- Sys.time()
+        cat("Total time taken, ", end_time-start_time, "\n")
+    })
+    ordlist
+}
+##' Top level function for buying and selling according to rules
+##'
+##' See above
+##' @title 
+##' @param level a level object
+##' @param ordertype the type of orders to place
+##' @return a list containing the orders placed
+##' @author richie
+market_make <- function(level, ordertype="limit", qty=NULL) {
+    if(level=="TEST") {
+        account <- "EXB123456"
+        venue <- "TESTEX"
+        ticker <- "FOOBAR"
+        balance <- NULL
+    }
+    else {
+    account <- get_component(level, "account")
+    venue <- get_component(level, "venues")
+    ticker <- get_component(level, "tickers")
+    balance <- get_component(level, "balances")
+    }
+    ## browser()
+    buys <- NA
+    sells <- NA
+    prices <- c(buys, sells)
+    while(any(is.na(prices))) {
+    orders <- get_orderbook(venue, ticker)
+    parsed <- orderbook(parse_response(orders))
+    buys <- floor(max(parsed@bids$price))
+    sells <- floor(max(parsed@asks$price))
+    prices <- c(buys, sells)
+    browser()
+    cat(prices, "\n")
+    }
+    directions <- c("buy", "sell")
+    if(is.null(qty)) {
+        qty <- 1
+    }
+    reslist <- list()
+    for(i in 1:length(prices)) {
+        ord<- create_order(account=account,
+                           venue=venue,
+                           stock=ticker,
+                           price=prices[i],
+                           qty=qty,
+                           direction=directions[i],
+                           ordertype="market")
+    placed <- place_order(venue=venue, stock=ticker, body=ord, apikey=apikey)
+    reslist[[i]] <- placed
+    }
+    names(reslist) <- directions
+    reslist
 }
